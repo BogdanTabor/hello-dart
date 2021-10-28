c658b5f0b645820df4b5d6fe475c892b1452d348
Bohdan Tabor <bogdan.tabor@gmail.com>
2021-10-28 19:44:48 +0300

    first commit

---
+++ hello.dart
@@ -0,0 +1,10 @@
+void main() {
+  String greetings = 'Hello';
+  String language = 'Dart';
+  String message = '${greetings}, ${language}';
+  printMsg(message);
+}
+
+void printMsg(msg) {
+  print(msg);
+}
